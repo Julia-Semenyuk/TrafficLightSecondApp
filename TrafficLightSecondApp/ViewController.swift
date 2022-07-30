737a03b4e6c2189797e13428b5399e52f98912a6
Julia Semenyuk <julia_semenyuk@internet.ru>
2022-07-31 01:54:33 +0900

    1

--- TrafficLightSecondApp/ViewController.swift
+++ TrafficLightSecondApp/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var trafficLightSettingsView: UIStackView!
+    @IBOutlet var trafficLightButtonView: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        trafficLightSettingsView.layer.cornerRadius = 50
+        trafficLightSettingsView.alpha = 0.2
     }
 
-
+    @IBAction func trafficLightButtonPressed() {
+        
+        for _ in 1...3 {
+         
+        }
+        
+    }
+    
 }
 
